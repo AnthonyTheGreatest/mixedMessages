ca953e071ddea7a2141cf68bf419ab257b4d1818
[Miklós Milosevits] <[milosevitsmiklos@gmail.com]>
2021-09-29 08:18:39 +0200

    Create 3 arrays for meal items nested in one object

--- code.js
+++ code.js
@@ -4,7 +4,13 @@ console.log('Hello');
 // -main item
 // -drink
 // -snack
-// Outpu example: Your meal today will be: Hamburger, Cola and a Snickers bar.
+// Outpu example: Your meal today will be: hamburger, Cola and a Snickers bar.
+
+const allItems = {
+    main: ['hamburger', 'lasagna', 'fish and chips', 'spaghetti', 'pizza'],
+    drink: ['Cola', 'Fanta', 'fizzy water', 'non-alcoholic beer'],
+    snack: ['Snickers bar', 'slice of cheesecake', 'slice of chocolate cake']
+};
 
 
 
